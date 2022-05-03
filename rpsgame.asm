f69ac2db617cbe817026d0c5867022a7994a6185
jman316b <jeremiahg316b@gmail.com>
2022-05-03 16:55:59 -0700

    fixed loops and computer choice

--- rpsgame.asm
+++ rpsgame.asm
@@ -50,48 +50,51 @@ main:
 	syscall
 	move $s1, $v0
 	
-	beq $s1, 0, exit
+	beq $s1, 1, exit
 	
-		#Telling the user what the choices are, should they accept to play the game
-		la $a0, enterChoice
-		li $v0, 4
-		syscall
+game:
+	#Telling the user what the choices are, should they accept to play the game
+	la $a0, enterChoice
+	li $v0, 4
+	syscall
+
+	la $a0, rock
+	li $v0, 4
+	syscall
+
+	la $a0, paper
+	li $v0, 4
+	syscall
+
+	la $a0, scissor
+	li $v0, 4
+	syscall
+
+	la $a0, userChoice
+	li $v0, 4
+	syscall
+
+	#User input choice
+	li $v0, 5
+	syscall
+	move $s0, $v0 # user choice is in $s0
 	
-		la $a0, rock
-		li $v0, 4
-		syscall
-	
-		la $a0, paper
-		li $v0, 4
-		syscall
-	
-		la $a0, scissor
-		li $v0, 4
-		syscall
-	
-		la $a0, userChoice
-		li $v0, 4
-		syscall
-	
-		#User input choice
-		li $v0, 5
- 		syscall
- 		move $s0, $v0 # user choice is in $s0
+	#Computer Choice
+	li $a1, 3 #this is to set the upper limit (in this case the limit is at 3
+	li $v0, 42 #syscall to create the random number
+	syscall #returns the random number at $a0
+	addi	$s1, $a0, 1			# computer choice is in $s1
 		
-		#Computer Choice
-		li $a1, 3 #this is to set the upper limit (in this case the limit is at 3
-		li $v0, 42 #syscall to create the random number
-		syscall #returns the random number at $a0
-		
-	beq $s1, 1, exit #Temporary
+	# Compare them and return result
+	# Ask to play again
 	
-	exit:
+exit:
 	la $a0, ending
 	li $v0, 4
 	syscall
-	
+
 	li $v0, 10
 	syscall
-	
+
 	
 		
